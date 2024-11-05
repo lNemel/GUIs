72d1fe742c14d32d4b22c7be546d2120d1bcae2f
DominicAmbro645 <143234977+DominicAmbro645@users.noreply.github.com>
2024-11-05 08:04:24 -0600

    Add files via upload

---
+++ frame.java
@@ -0,0 +1,12 @@
+
+import javax.swing.JFrame;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		JFrame frame = new JFrame("Hello 					frame!!");
+        frame.setDefaultCloseOperation(JFrame.		EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.setSize(400, 400);
+	}
+}
