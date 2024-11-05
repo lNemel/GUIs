e43b036d37076a5db90b78603e26b12389c16695
Santiago Yael Cortés Álvarez <yael.cortes.alvarez@gmail.com>
2024-11-05 07:55:08 -0600

    Add files via upload

--- MyJFrame.java
+++ MyJFrame.java
@@ -1,32 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
-package com.mycompany.guiss;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-/**
- *
- * @author efcru
- */
-
 
 public class MyJFrame extends JFrame {
     private JLabel myLabel;
 
     public MyJFrame() {
-        myLabel = new JLabel();
-        myLabel.setText("Hola Mundo");
-        add(myLabel);
+       
+        myLabel = new JLabel("Hola Mundo");
         
-        setTitle("Mi Ventana");
-        setSize(300, 200);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
+        this.add(myLabel);
+        
+        this.setSize(300, 200);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
     }
 
     public static void main(String[] args) {
         new MyJFrame();
     }
 }
-
